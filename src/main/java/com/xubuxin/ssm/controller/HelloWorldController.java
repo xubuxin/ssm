11ce317d9e2d48a80235452956492ab9c8211972
xubuxin <xubuxin@yahoo.com>
2019-04-20 13:16:10 +0800

    add logback

--- src/main/java/com/xubuxin/ssm/controller/HelloWorldController.java
+++ src/main/java/com/xubuxin/ssm/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.xubuxin.ssm.controller;
 
 import com.xubuxin.ssm.bean.Person;
 import com.xubuxin.ssm.mapper.HelloWorldMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +19,15 @@ import java.util.List;
 @RestController
 public class HelloWorldController {
 
+    private static Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Autowired
     HelloWorldMapper helloWorldMapper;
 
     @RequestMapping(value = "/people", method = RequestMethod.GET)
     public List<Person> selectPersons(){
         List<Person> people = helloWorldMapper.selectPeople();
+        logger.info("select all people!");
         return people;
     }
 }
